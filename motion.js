d96689182a5b527fea9596f034a0c94b772d7a8b
Volume00 <130169418+Volume00@users.noreply.github.com>
2024-01-14 15:31:44 +0100

    Add files via upload

--- motion.js
+++ motion.js
@@ -1,6 +1,10 @@
 function apriLettera() {
     var lettera = document.getElementById("lettera");
     var contenuto = document.getElementById("contenuto");
+    var music = document.getElementById("music");
+
+        
+        music.play();
   
     lettera.style.transform = "rotateY(-180deg)";
     lettera.style.transition = "transform 0.5s";
